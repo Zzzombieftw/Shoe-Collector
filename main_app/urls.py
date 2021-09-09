266298f31bbc5e491b36badc972656b76cf6989a
Zahir <zahir.spencer@yahoo.com>
2021-09-09 12:33:14 -0400

    stlying

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('shoes/create/', views.ShoeCreate.as_view(), name='shoes_create'),
     path('shoes/<int:pk>/update/', views.ShoeUpdate.as_view(), name='shoes_update'),
     path('shoes/<int:pk>/delete/', views.ShoeDelete.as_view(), name='shoes_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Shoe
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create the view
 class Home(LoginView):
